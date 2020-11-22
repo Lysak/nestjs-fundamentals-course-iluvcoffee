ecded839159a576cf7fa5d914af1dffca528185d
Dmytrii Lysak <dlysak@live.com>
2020-11-22 16:13:10 +0200

    12

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,15 +1,40 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get()
-  findAll() {
-    return 'This action returns all coffees';
+  findAll(@Query() paginationQuery) {
+    const { limit, offset } = paginationQuery;
+    return `This action returns all coffees. Limit ${limit}, ${offset}`;
   }
 
   @Get(':id')
   findOne(@Param('id') id: number) {
     // only int ??
     return `This action returns #${id} coffee`;
+  }
+
+  @Post(':id')
+  create(@Body() body) {
+    return body;
+    // return `This action creates a coffee`;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `This action updates #${id} coffee`;
+  }
+
+  @Patch(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes #${id} coffee`;
   }
 }
