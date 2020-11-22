aff578ed43dd574f9a78019ec16cf441879b61cd
Dmytrii Lysak <dlysak@live.com>
2020-11-22 14:28:51 +0200

    8

---
+++ src/coffees/coffees.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, Param } from '@nestjs/common';
+
+@Controller('coffees')
+export class CoffeesController {
+  @Get()
+  findAll() {
+    return 'This action returns all coffees';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    // only int ??
+    return `This action returns #${id} coffee`;
+  }
+}
