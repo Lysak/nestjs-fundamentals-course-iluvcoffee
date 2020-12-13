731ad12e33e5f8d8cd9df6fdcce0ad83ac2c8eca
Dmytrii Lysak <dlysak@live.com>
2020-12-13 16:13:55 +0200

    14 add exception

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { Coffee } from './entities/coffee.entity';
 
 @Injectable()
@@ -17,7 +17,11 @@ export class CoffeesService {
   }
 
   findOne(id: string) {
-    return this.coffees.find(item => item.id === +id);
+    const coffee = this.coffees.find(item => item.id === +id);
+    if (!coffee) {
+      throw new HttpException(`Coffee #${id} not found`, HttpStatus.NOT_FOUND);
+    }
+    return coffee;
   }
 
   create(createCoffeeDto: any) {
