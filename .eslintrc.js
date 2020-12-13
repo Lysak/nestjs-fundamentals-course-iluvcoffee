a362f801610714cd0e211d7f5b363d1ef1d4177d
Dmytrii Lysak <dlysak@live.com>
2020-12-13 15:43:02 +0200

    clean code

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,9 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    // project: ['./tsconfig.json'],
-    debugLevel: true, // tmp
-
+    // project: 'tsconfig.json',
     sourceType: 'module',
-
-    tsconfigRootDir: __dirname,
+    // debugLevel: true, // tmp
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
@@ -14,18 +11,22 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'prettier',
     'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
   ],
   root: true,
   env: {
     node: true,
+    jest: true,
   },
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+
+    // test
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
     '@typescript-eslint/ban-types': 'off',
-    "semi": [2, "always"], //test
+    // semi: [2, 'always'], //test
   },
 };
