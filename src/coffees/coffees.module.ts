4d1f1d4df70dfed57150f4cce1d5d6a87c2437c6
Dmytrii Lysak <dlysak@live.com>
2020-12-14 22:34:47 +0200

    24

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,6 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Coffee } from './entities/coffee.entity';
 
-@Module({ controllers: [CoffeesController], providers: [CoffeesService] })
+@Module({
+  imports: [TypeOrmModule.forFeature([Coffee])],
+  controllers: [CoffeesController],
+  providers: [CoffeesService],
+})
 export class CoffeesModule {}
