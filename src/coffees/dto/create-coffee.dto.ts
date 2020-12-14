69421c81f9013c1f410afdfda4a95d83ddd90fa3
Dmytrii Lysak <dlysak@live.com>
2020-12-14 21:06:18 +0200

    17

--- src/coffees/dto/create-coffee.dto.ts
+++ src/coffees/dto/create-coffee.dto.ts
@@ -1,6 +1,13 @@
+import { IsString } from 'class-validator';
+
 export class CreateCoffeeDto {
   //nest g class coffees/dto/create-coffee.dto --no-spec
+  @IsString()
   readonly name: string;
+
+  @IsString()
   readonly brand: string;
+
+  @IsString({ each: true })
   readonly flavour: string[];
 }
--- src/coffees/dto/update-coffee.dto.ts
+++ src/coffees/dto/update-coffee.dto.ts
@@ -1,6 +1,4 @@
-export class UpdateCoffeeDto {
-  //nest g class coffees/dto/update-coffee.dto --no-spec
-  readonly name?: string;
-  readonly brand?: string;
-  readonly flavour?: string[];
-}
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateCoffeeDto } from './create-coffee.dto';
+
+export class UpdateCoffeeDto extends PartialType(CreateCoffeeDto) {}
