7d6bee47dc54eadfd6bb761abde1b49287e848e5
Dmytrii Lysak <dlysak@live.com>
2020-12-20 10:41:33 +0200

    config heroku #1.2

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,16 +8,25 @@ import { TypeOrmModule } from '@nestjs/typeorm';
   imports: [
     CoffeesModule,
     // secure in future
-    TypeOrmModule.forRoot({
-      type: 'postgres' as const, // type of our database
-      host: 'localhost', // database host
-      port: 5432, // database port
-      username: 'postgres', // username
-      password: 'pass123', // user password
-      database: 'postgres', // name of our database,
-      autoLoadEntities: true, // models will be loaded automatically (you don't have to explicitly specify the entities: [] array)
-      synchronize: true, // your entities will be synced with the database (ORM will map entity definitions to corresponding SQL tabled), every time you run the application (recommended: disable in the production)
-    }),
+    TypeOrmModule.forRoot(
+      process.env.DATABASE_URL
+        ? {
+            type: 'postgres' as const, // type of our database
+            autoLoadEntities: true, // models will be loaded automatically (you don't have to explicitly specify the entities: [] array)
+            synchronize: true, // your entities will be synced with the database (ORM will map entity definitions to corresponding SQL tabled), every time you run the application (recommended: disable in the production)
+            url: process.env.DATABASE_URL,
+          }
+        : {
+            type: 'postgres' as const, // type of our database
+            host: 'localhost', // database host
+            port: 5432, // database port
+            username: 'postgres', // username
+            password: 'pass123', // user password
+            database: 'postgres', // name of our database,
+            autoLoadEntities: true, // models will be loaded automatically (you don't have to explicitly specify the entities: [] array)
+            synchronize: true, // your entities will be synced with the database (ORM will map entity definitions to corresponding SQL tabled), every time you run the application (recommended: disable in the production)
+          },
+    ),
   ],
   controllers: [AppController],
   providers: [AppService],
