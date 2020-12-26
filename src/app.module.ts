2ed78abd621154aeafa65c9e7e80b69d4f40d5ae
Dmytrii Lysak <dlysak@live.com>
2020-12-26 22:07:45 +0200

    44 #2

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,16 +13,25 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule.forRoot(),
     CoffeesModule,
     // secure in future
-    TypeOrmModule.forRoot({
-      type: 'postgres' as const, // type of our database
-      host: process.env.DATABASE_HOST, // database host
-      port: +process.env.DATABASE_PORT, // database port
-      username: process.env.DATABASE_USER, // username
-      password: process.env.DATABASE_PASSWORD, // user password
-      database: process.env.DATABASE_NAME, // name of our database,
-      autoLoadEntities: true, // models will be loaded automatically (you don't have to explicitly specify the entities: [] array)
-      synchronize: false, // your entities will be synced with the database (ORM will map entity definitions to corresponding SQL tabled), every time you run the application (recommended: disable in the production)
-    }),
+    TypeOrmModule.forRoot(
+      process.env.DATABASE_URL
+        ? {
+            type: 'postgres' as const,
+            url: process.env.DATABASE_URL,
+            autoLoadEntities: true,
+            synchronize: false,
+          }
+        : {
+            type: 'postgres' as const, // type of our database
+            host: process.env.DATABASE_HOST, // database host
+            port: +process.env.DATABASE_PORT, // database port
+            username: process.env.DATABASE_USER, // username
+            password: process.env.DATABASE_PASSWORD, // user password
+            database: process.env.DATABASE_NAME, // name of our database,
+            autoLoadEntities: true, // models will be loaded automatically (you don't have to explicitly specify the entities: [] array)
+            synchronize: false, // your entities will be synced with the database (ORM will map entity definitions to corresponding SQL tabled), every time you run the application (recommended: disable in the production)
+          },
+    ),
     DatabaseModule,
     CoffeeRatingModule,
   ],
