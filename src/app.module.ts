a9b85117376d46f616cf24c57fdae4f332623462
Dmytrii Lysak <dlysak@live.com>
2020-12-26 21:43:12 +0200

    44

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,30 +6,23 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { DatabaseModule } from './database/database.module';
 import { CoffeeRatingService } from './coffee-rating/coffee-rating.service';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     CoffeesModule,
     // secure in future
-    TypeOrmModule.forRoot(
-      process.env.DATABASE_URL
-        ? {
-            type: 'postgres' as const, // type of our database
-            autoLoadEntities: true, // models will be loaded automatically (you don't have to explicitly specify the entities: [] array)
-            synchronize: true, // your entities will be synced with the database (ORM will map entity definitions to corresponding SQL tabled), every time you run the application (recommended: disable in the production)
-            url: process.env.DATABASE_URL,
-          }
-        : {
-            type: 'postgres' as const, // type of our database
-            host: 'localhost', // database host
-            port: 5432, // database port
-            username: 'postgres', // username
-            password: 'pass123', // user password
-            database: 'postgres', // name of our database,
-            autoLoadEntities: true, // models will be loaded automatically (you don't have to explicitly specify the entities: [] array)
-            synchronize: false, // your entities will be synced with the database (ORM will map entity definitions to corresponding SQL tabled), every time you run the application (recommended: disable in the production)
-          },
-    ),
+    TypeOrmModule.forRoot({
+      type: 'postgres' as const, // type of our database
+      host: process.env.DATABASE_HOST, // database host
+      port: +process.env.DATABASE_PORT, // database port
+      username: process.env.DATABASE_USER, // username
+      password: process.env.DATABASE_PASSWORD, // user password
+      database: process.env.DATABASE_NAME, // name of our database,
+      autoLoadEntities: true, // models will be loaded automatically (you don't have to explicitly specify the entities: [] array)
+      synchronize: false, // your entities will be synced with the database (ORM will map entity definitions to corresponding SQL tabled), every time you run the application (recommended: disable in the production)
+    }),
     DatabaseModule,
     CoffeeRatingModule,
   ],
