ce1693f1c7d025b5a8c69f135ccb5972657f7c04
Dmytrii Lysak <dlysak@live.com>
2020-12-28 23:01:08 +0200

    clean code

--- src/coffee-rating/coffee-rating.module.ts
+++ src/coffee-rating/coffee-rating.module.ts
@@ -6,11 +6,19 @@ import { DatabaseModule } from '../database/database.module';
 @Module({
   imports: [
     DatabaseModule.register({
-      type: 'postgres' as const, // type of our database
-      host: process.env.DATABASE_HOST, // database host
-      username: process.env.DATABASE_USERNAME, // username
-      password: process.env.DATABASE_PASSWORD, // user password
-      port: +process.env.DATABASE_PORT, // database port
+      type: 'postgres',
+      host: 'localhost',
+      username: 'postgres',
+      password: 'pass123',
+      port: 5432,
+
+      // tmp fix in future
+
+      // type: 'postgres' as const, // type of our database
+      // host: process.env.DATABASE_HOST, // database host
+      // username: process.env.DATABASE_USERNAME, // username
+      // password: process.env.DATABASE_PASSWORD, // user password
+      // port: +process.env.DATABASE_PORT, // database port
     }),
     CoffeesModule,
   ],
