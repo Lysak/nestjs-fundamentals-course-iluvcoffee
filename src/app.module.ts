a2d508040047f6e208933174338a2d38693e05b4
Dmytrii Lysak <dlysak@live.com>
2021-01-03 21:06:53 +0200

    50

--- src/app.module.ts
+++ src/app.module.ts
@@ -27,25 +27,26 @@ import appConfig from './config/app.config';
     }),
     CoffeesModule,
     // secure in future
-    TypeOrmModule.forRoot(
-      process.env.DATABASE_URL
-        ? {
-            type: 'postgres' as const,
-            url: process.env.DATABASE_URL,
-            autoLoadEntities: true,
-            synchronize: false,
-          }
-        : {
-            type: 'postgres' as const, // type of our database
-            host: process.env.DATABASE_HOST, // database host
-            port: +process.env.DATABASE_PORT, // database port
-            username: process.env.DATABASE_USERNAME, // username
-            password: process.env.DATABASE_PASSWORD, // user password
-            database: process.env.DATABASE_NAME, // name of our database,
-            autoLoadEntities: true, // models will be loaded automatically (you don't have to explicitly specify the entities: [] array)
-            synchronize: false, // your entities will be synced with the database (ORM will map entity definitions to corresponding SQL tabled), every time you run the application (recommended: disable in the production)
-          },
-    ),
+    TypeOrmModule.forRootAsync({
+      useFactory: () =>
+        process.env.DATABASE_URL
+          ? {
+              type: 'postgres' as const,
+              url: process.env.DATABASE_URL,
+              autoLoadEntities: true,
+              synchronize: false,
+            }
+          : {
+              type: 'postgres' as const, // type of our database
+              host: process.env.DATABASE_HOST, // database host
+              port: +process.env.DATABASE_PORT, // database port
+              username: process.env.DATABASE_USERNAME, // username
+              password: process.env.DATABASE_PASSWORD, // user password
+              database: process.env.DATABASE_NAME, // name of our database,
+              autoLoadEntities: true, // models will be loaded automatically (you don't have to explicitly specify the entities: [] array)
+              synchronize: false, // your entities will be synced with the database (ORM will map entity definitions to corresponding SQL tabled), every time you run the application (recommended: disable in the production)
+            },
+    }),
     DatabaseModule,
     CoffeeRatingModule,
   ],
