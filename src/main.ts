a3d8c7d72f4caf82e1db414b20983616ef1fff1c
Dmytrii Lysak <dlysak@live.com>
2021-01-07 22:45:59 +0200

    53

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,7 @@ async function bootstrap() {
       },
     }),
   );
+  app.useGlobalFilters(new HttpExceptionFilter());
   app.enableCors();
   await app.listen(process.env.PORT || 3000);
 }
