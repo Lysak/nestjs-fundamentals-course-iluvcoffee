c4df509a3b357dcaddaba4611aeb3bf83b3f2029
Dmytrii Lysak <dlysak@live.com>
2021-01-08 23:58:42 +0200

    54 test

---
+++ src/common/guards/api-key.guard.ts
@@ -0,0 +1,15 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class ApiKeyGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest<Request>();
+    // const authHeader = request.header('Authorization');
+
+    // return authHeader === process.env.API_KEY;
+    return true;
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { ApiKeyGuard } from './common/guards/api-key.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,6 +18,7 @@ async function bootstrap() {
   );
   app.useGlobalFilters(new HttpExceptionFilter());
   app.enableCors();
+  app.useGlobalGuards(new ApiKeyGuard());
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
