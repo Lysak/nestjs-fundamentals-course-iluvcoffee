2ae51614744b93953e4f3d858c0b6ef9ba42c2e8
Dmytrii Lysak <dlysak@live.com>
2021-01-09 00:02:42 +0200

    54 ++

--- src/common/guards/api-key.guard.ts
+++ src/common/guards/api-key.guard.ts
@@ -1,15 +1,30 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { Request } from 'express';
+import { Reflector } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
 
 @Injectable()
 export class ApiKeyGuard implements CanActivate {
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly configService: ConfigService,
+  ) {}
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    const request = context.switchToHttp().getRequest<Request>();
-    // const authHeader = request.header('Authorization');
+    // getHandler references the method that will be triggered after going through here
+    // reflector is used to get metadata that is set using SetMetadata
+    const isPublic = this.reflector.get(IS_PUBLIC_KEY, context.getHandler());
 
-    // return authHeader === process.env.API_KEY;
-    return true;
+    if (isPublic) return true;
+
+    // switchToHttp give us access to request, response, and next
+    const request = context.switchToHttp().getRequest<Request>();
+    const authHeader = request.header('Authorization');
+
+    // instead of using process.env would rather use config
+    return authHeader === this.configService.get('API_KEY');
   }
 }
