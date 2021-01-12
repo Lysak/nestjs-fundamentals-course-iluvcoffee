d990d64c889d81462e77be4f10ee3baa012a6085
Dmytrii Lysak <dlysak@live.com>
2021-01-12 23:04:06 +0200

    56

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { WrapResponseInterceptor } from './common/interceptors/wrap-response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,6 +18,7 @@ async function bootstrap() {
   );
   app.useGlobalFilters(new HttpExceptionFilter());
   app.enableCors();
+  app.useGlobalInterceptors(new WrapResponseInterceptor());
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
