c80a7a179538c2066f701b6d9a30a3082fa5ceb3
Dmytrii Lysak <dlysak@live.com>
2021-01-14 23:51:20 +0200

    59

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,10 +1,25 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { ApiKeyGuard } from './guards/api-key.guard';
 import { ConfigModule } from '@nestjs/config';
+import { LoggingMiddleware } from './middleware/logging.middleware';
 
 @Module({
   imports: [ConfigModule],
   providers: [{ provide: APP_GUARD, useClass: ApiKeyGuard }],
 })
-export class CommonModule {}
+export class CommonModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): any {
+    consumer
+      .apply(LoggingMiddleware)
+      // .exclude('coffees')
+      .forRoutes('*');
+    // 'coffees'
+    // {path: 'coffees', method: RequestMethod.GET }
+  }
+}
