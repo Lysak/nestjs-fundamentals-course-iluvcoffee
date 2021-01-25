6883f3a45dcc8f012b6102c18003bbff01a10363
Dmytrii Lysak <dlysak@live.com>
2021-01-25 21:22:18 +0200

    75

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    MongooseModule.forRoot('mongodb://localhost:27018/nest-course'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
